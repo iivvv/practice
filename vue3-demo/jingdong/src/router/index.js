86baabcdbf80a8afe2d41b8d251ef9230234b9d7
iivvv <734994096@qq.com>
2022-11-24 17:53:32 +0800

    router

--- vue3-demo/jingdong/src/router/index.js
+++ vue3-demo/jingdong/src/router/index.js
@@ -1,3 +1,4 @@
+import { nextTick } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../views/home/Home'
 import LoginPage from '../views/login/Login'
@@ -18,6 +19,20 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+// 路由守卫设置
+// 每次路由跳转之前
+// to是去哪个目录，from是从哪个目录
+router.beforeEach((to, from, next) => {
+  // console.log(to, from)
+  const isLogin = localStorage.isLogin
+  // 避免死循环
+  if (isLogin || to.name === 'Login') {
+    next()
+  } else {
+    next({ name: 'Login' })
+  }
 })
 
 export default router
